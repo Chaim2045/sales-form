6a0d9098e62c24ba6d195bafd4f6d350e77b536a
Chaim2045 <chaim2045@users.noreply.github.com>
2026-01-19 18:40:59 +0200

    Update Firebase credentials to new project

--- env-config.js
+++ env-config.js
@@ -7,14 +7,14 @@
 
     // Production configuration
     window.ENV_CONFIG = {
-        FIREBASE_API_KEY: 'AIzaSyC9R_eupXtdkzEMBwA1Dsc6SC_14_iUNLs',
-        FIREBASE_AUTH_DOMAIN: 'law-office-guide.firebaseapp.com',
-        FIREBASE_DATABASE_URL: 'https://law-office-guide-default-rtdb.europe-west1.firebasedatabase.app',
-        FIREBASE_PROJECT_ID: 'law-office-guide',
-        FIREBASE_STORAGE_BUCKET: 'law-office-guide.firebasestorage.app',
-        FIREBASE_MESSAGING_SENDER_ID: '903121364456',
-        FIREBASE_APP_ID: '1:903121364456:web:91d02f021ab618d3a6705d',
-        FIREBASE_MEASUREMENT_ID: 'G-3NZXL9YB35',
+        FIREBASE_API_KEY: 'AIzaSyAkRGg1HUaJhimwIhRir7wQ0vrZRUuqIy8',
+        FIREBASE_AUTH_DOMAIN: 'law-office-sales-form.firebaseapp.com',
+        FIREBASE_DATABASE_URL: 'https://law-office-sales-form-default-rtdb.firebaseio.com',
+        FIREBASE_PROJECT_ID: 'law-office-sales-form',
+        FIREBASE_STORAGE_BUCKET: 'law-office-sales-form.firebasestorage.app',
+        FIREBASE_MESSAGING_SENDER_ID: '120096251777',
+        FIREBASE_APP_ID: '1:120096251777:web:c88b4d0b3d13a5713f9280',
+        FIREBASE_MEASUREMENT_ID: '',
         GOOGLE_SHEETS_WEBHOOK: 'https://script.google.com/macros/s/AKfycbx4en4xw-4cG7_ytYE66rLswHCoV8JDwg8g5-QL9geMFhhIdYY-2Qhw_ZgTR3R_e-7l/exec'
     };
 })();
